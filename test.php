b28a210b20a98fc7fc1d4b551059279fb4426054
Jason Birdwell <jasonbirdwell@Jasons-MacBook-Pro-3.local>
2014-10-02 14:00:03 -0500

    blah blah

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+	
+	$cars = [
+
+		'Isaac' => [
+			'make' => 'Toyota',
+			'model' => 'Camry',
+			'year' => 2001
+		],
+
+		'Liz' => [
+			'make' => 'Ford',
+			'model' => 'Focus',
+			'year' => 2012
+		],
+
+		'Margaret' => [
+			'make' => 'Chevy',
+			'model' => 'Malibu',
+			'year' => 2007
+		]
+
+	];
+
+// Print Isaac Model
+echo $cars ['Isaac']['model'];	
