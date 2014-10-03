862e62a992079951510171b7c2619d9b80bd908e
Jason Birdwell <jasonbirdwell@Jasons-MacBook-Pro-3.local>
2014-10-03 15:44:38 -0500

    doing some whiles

---
+++ dowhile.php
@@ -0,0 +1,32 @@
+<?php
+
+// $count = 1;
+// do {
+// 		echo $count . ' bat... aaaah';
+// }
+
+
+// $a = 5;
+// do {
+// 		echo "\$a is equal to {$a}\n";
+// 		$a++;
+// }	while ($a <= 5);
+
+// while ($a <= 5) {
+// 	echo "\$a is equal to {$a}\n";
+// 	$a++;
+// }
+
+// $a = 1;
+// do {
+//     echo "\$a is equal to {$a}\n";
+//     $a++;
+// } while (1 == 2);
+
+$count = 2;
+do {
+	echo "\$count is equal to {$count}\n";
+	$count*=2;
+} while ($count <= 1000000);
+
+?>
