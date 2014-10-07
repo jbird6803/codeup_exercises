c0b6639c88b5815a1a53eec9e59406b939202a82
Jason Birdwell <jasonbirdwell@Jasons-MacBook-Pro-3.local>
2014-10-07 11:50:05 -0500

    break continue

---
+++ break_continue.php
@@ -0,0 +1,51 @@
+<?php
+
+// for ($i = 1; $i <= 100; $i++) {
+//     echo $i . "\n";
+//     if ($i == 5) {
+//         break;
+//     }
+// }
+
+// echo 'test';
+
+
+// for ($i = 1; $i <= 10; $i++) {
+//     echo $i . "\n";
+//     if ($i % 2 == 0) {
+//         continue;
+//     }
+//     echo "^ that is an odd number.\n";
+// }
+
+
+
+// $numbers = array(1, 2, 3, 4, 5);
+// // Loop 1
+// foreach ($numbers as $key => $value) {
+//     echo "{$value}\n";
+//     // Loop 2
+//     for ($i = 1; $i <= 10; $i++) {
+//         if ($i == 2) {
+//             echo "{$i}\n";
+//             break 2;
+//         }
+//     }
+// }
+// echo "done!\n";
+
+
+for ($a = 1; $a <= 100; $a++) {
+	if ($a % 2 == 0) {
+		echo $a . "\n";
+	}
+	if ($a == 10) {
+		break;
+	}
+}
+
+
+
+
+
+
